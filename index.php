52a37a71035554987429614ae025aaa3c850392e
Tristan Bennett <tmbennett2011@msn.com>
2018-01-10 08:18:19 -0700

    info added to table

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Snap Tables</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<h1># Web Languages</h1>
+		<table>
+			<tr>
+				<th>Language</th>
+				<th>Number of Users</th>
+				<th>Downside</th>
+			</tr>
+			<tr>
+				<td>Java</td>
+				<td>125,718,403</td>
+				<td>Frameworks revolt! It's Java Spring</td>
+			</tr>
+			<tr>
+				<td>.NET</td>
+				<td>64,371,4733</td>
+				<td>PHP forces to clean twice per week</td>
+			</tr>
+			<tr>
+				<td>PHP</td>
+				<td>75,383,142</td>
+				<td>Misunderstood by Java and .NET Developers</td>
+			</tr>
+			<tr>
+				<td>Ruby</td>
+				<td>1,403</td>
+				<td>Only for Hipster Driven Development (HDD)</td>
+			</tr>
+		</table>
+	</body>
+</html>
